00ae7a5ae94e3cbbf665d781685c6aed1a64f8ae
Bucky <singharpan09@gmail.com>
2020-05-02 20:26:12 +0530

    token is authenticated

--- src/admin.jsx
+++ src/admin.jsx
@@ -1,12 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-const Admin = () => {
-  return (
-    <>
-      <h1>Welcome to admin ...Only Auth people are allowed here</h1>;
-      <Link to="/logout">Logout</Link>
-    </>
-  );
-};
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+//Will check the token in constructor in Admin page
+//now directlty redirecting to admin link will not work
+//token get stored in local storage once we login
+class Admin extends Component {
+  constructor(props) {
+    super(props);
+    const token = localStorage.getItem("token");
+    let loggedIn = true;
+    console.log(token);
+    if (token === null) {
+      loggedIn = false;
+    }
+    this.state = {
+      loggedIn,
+    };
+  }
+  render() {
+    if (this.state.loggedIn === false) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <>
+        <h1>Welcome to admin ...Only Auth people are allowed here</h1>;
+        <Link to="/logout">Logout</Link>
+      </>
+    );
+  }
+}
 export default Admin;
