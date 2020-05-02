df1af02895dd5f9026f06968a730f7eddfe7ea7b
Bucky <singharpan09@gmail.com>
2020-05-02 19:36:35 +0530

    switch wrapped with browser router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import List from "./listcomponent";
 import NavBar from "./navbar";
 import About from "./about";
 import Contact from "./contact";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "./login";
 import Logout from "./logout";
 import Admin from "./admin";
@@ -13,16 +13,17 @@ import Admin from "./admin";
 function App() {
   return (
     <div>
-      <NavBar />
-
+      {/* <NavBar /> */}
       <BrowserRouter>
-        <Route path="/admin" component={Admin} />
-        <Route path="/" component={Login} />
-        <Route path="/logout" component={Logout} />
+        <Switch>
+          <Route path="/admin" component={Admin} />
+          <Route exact path="/" component={Login} />
+          <Route path="/logout" component={Logout} />
 
-        {/* <Route path="/list" component={List} />
+          {/* <Route path="/list" component={List} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} /> */}
+        </Switch>
       </BrowserRouter>
     </div>
   );
