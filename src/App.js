b10d6fea2526be2709b3e15f558b8a4fe6b9fcf3
Bucky <singharpan09@gmail.com>
2020-05-02 18:51:32 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import List from "./listcomponent";
 import NavBar from "./navbar";
 import About from "./about";
 import Contact from "./contact";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <List />
-      <About />
-      <Contact />
+
+      <BrowserRouter>
+        <Route path="/list" component={List} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </BrowserRouter>
     </div>
   );
 }
